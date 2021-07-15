c019d261d95737b6f1fe5c72727e647f81ea1e20
NazDg <nyzirr@gmail.com>
2021-07-15 12:54:54 -0500

    finished?

--- index.js
+++ index.js
@@ -149,9 +149,9 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
-} 
+function getLastReview(array) {
+  console.log(`${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} start rating and their feedback was : "${array[array.length-1].feedback}".`);
+  }
 
 
 
